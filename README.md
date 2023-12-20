224b60c24e391ed8a6946634b066b2154e444563
Kartones <diego@kartones.net>
2023-12-20 14:29:41 +0100

    breaking change: new versions have different checksum secret

--- README.md
+++ README.md
@@ -2,16 +2,19 @@
 
 Tiny NodeJS application to fiddle with the game save files.
 
+⚠⚠⚠ With the migration to Unity3D (late 2023), the game changed the savegame's checksum secret and this tool no longer works ⚠⚠⚠
+⚠⚠⚠ If I'm able to obtain the new secret and calculate valid checksums again, I'll update accordingly ⚠⚠⚠
+
 ## Features
 
-- Sets coins to 5,000,000
+- Sets coins to either 5000000 or the value that you pass as argument
 - Removes "bad eggs" from your characters. Currently only those that increase the `curse` value
 
 ## Instructions
 
 1) Either disable Steam cloud saves for the game, or in case of conflict, always choose local files
 2) Drop this file into the game's save folder. For versions prior to `1.0`, check [PCGamingWiki](https://www.pcgamingwiki.com/wiki/Vampire_Survivors#Save_game_data_location); for the current version, check *Method 2* folder location of [this Steam Guide](https://steamcommunity.com/sharedfiles/filedetails/?id=2847140637).
-3) Run the file from a command line (`node vs-savegame-editor.mjs`)
+3) Run the file from a command line (`node vs-savegame-editor.mjs <coins-amount>`)
 4) Run the game. If coins are not changed, reset your power-ups
 
 You can edit the amount of coins that will be set by editing the `NEW_COINS_AMOUNT` constant at the beginning of `vs-savegame-editor.js`. Note that the script sets the current amount of coins to this value, but adds to the "lifetime coins" total, so if you reset your power-ups you recover the lifetime amount.
--- vs-savegame-editor.mjs
+++ vs-savegame-editor.mjs
@@ -2,11 +2,55 @@ import { createHash } from "crypto";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 
-// This is the amount of coins that will be added to the save game
-const NEW_COINS_AMOUNT = 5000000;
+// This is the amount of coins that will be added to the save game, if not passed as command-line argument
+const DEFAULT_NEW_COINS_AMOUNT = 5000000;
+
+// Useful for when you want to change other parameters of the save file by yourself, and still want to have a valid
+// checksum generated at the end
+const SKIP_CHECKSUM = false;
 
 const SAVE_FILENAME = "SaveData";
 const SAVE_FILENAME_OLD = "SaveData.sav";
+
+const main = () => {
+  if (isOldFileName()) {
+    console.log("Savegame is too old.");
+    return;
+  }
+
+  let newCoinsAmount = DEFAULT_NEW_COINS_AMOUNT;
+  if (process.argv.length === 3) {
+    try {
+      newCoinsAmount = parseInt(process.argv[2], 10);
+    } catch {
+      // Just keep the default
+    }
+  }
+
+  let saveGameData = loadSaveData(SAVE_FILENAME);
+
+  // Ensure that we can modify the save file (generate a valid checksum)
+  let validChecksum = isChecksumValid(saveGameData);
+
+  if (!SKIP_CHECKSUM) {
+    console.log("checksum pre-check correct?", validChecksum);
+  }
+
+  if (validChecksum || SKIP_CHECKSUM) {
+    createBackup(SAVE_FILENAME, saveGameData);
+
+    let saveData = changeAvailableCoins(saveGameData, newCoinsAmount);
+    saveData = removeBadEggs(saveData);
+
+    saveData["checksum"] = calculateChecksum(saveData);
+    saveDataToFile(saveData, SAVE_FILENAME);
+    console.log(`Modified game saved as '${SAVE_FILENAME}'`);
+  } else {
+    console.log(
+      "Can't regenerate checksum, no changes made to avoid savegame corruption."
+    );
+  }
+};
 
 const isOldFileName = () => {
   // Test first with the new filename, in case there are both files
@@ -63,13 +107,25 @@ const changeAvailableCoins = (saveData, desiredAmount) => {
   const saveDataCopy = copyData(saveData);
   saveDataCopy["checksum"] = "";
 
+  // Main game
   saveDataCopy["LifetimeCoins"] -= saveDataCopy["Coins"];
   saveDataCopy["Coins"] = desiredAmount;
   saveDataCopy["LifetimeCoins"] += desiredAmount;
 
+  // Adventures
+  Object.keys(saveDataCopy["AdventuresSaveData"]).forEach((adventureId) => {
+    saveDataCopy["AdventuresSaveData"][adventureId]["ADV_LifetimeCoins"] -=
+      saveDataCopy["AdventuresSaveData"][adventureId]["ADV_Coins"];
+    saveDataCopy["AdventuresSaveData"][adventureId]["ADV_Coins"] =
+      desiredAmount;
+    saveDataCopy["AdventuresSaveData"][adventureId]["ADV_LifetimeCoins"] +=
+      desiredAmount;
+  });
+
   return saveDataCopy;
 };
 
+// TODO: expand to adventures (`AdventuresSaveData` -> <Adventure-Id> -> `ADV_EggData`)
 const removeBadEggs = (saveData) => {
   // right now only one bad type, but allowing for future additions
   const badEggs = ["curse"];
@@ -88,24 +144,6 @@ const removeBadEggs = (saveData) => {
   return saveDataCopy;
 };
 
-// --- MAIN ---
+// ---
 
-let fileName = isOldFileName() ? SAVE_FILENAME_OLD : SAVE_FILENAME;
-let saveGameData = loadSaveData(fileName);
-
-// Ensure that we can modify the save file (generate a valid checksum)
-let validChecksum = isChecksumValid(saveGameData);
-console.log("checksum correct?", validChecksum);
-
-if (validChecksum) {
-  createBackup(fileName, saveGameData);
-
-  let saveData = changeAvailableCoins(saveGameData, NEW_COINS_AMOUNT);
-  if (!isOldFileName()) {
-    saveData = removeBadEggs(saveData);
-  }
-
-  saveData["checksum"] = calculateChecksum(saveData);
-  saveDataToFile(saveData, fileName);
-  console.log(`Modified game saved as '${fileName}'`);
-}
+main();
